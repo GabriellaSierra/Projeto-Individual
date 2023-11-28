7199821be4bdc8a390a26ba3a40f99563c9bcdff
GabriellaSierra <142419939+GabriellaSierra@users.noreply.github.com>
2023-11-27 21:32:04 -0300

    cadastro integrado com o banco de dados

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -56,7 +56,6 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var empresaId = req.body.empresaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -65,12 +64,10 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (empresaId == undefined) {
-        res.status(400).send("Sua empresa está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, empresaId)
+        usuarioModel.cadastrar(nome, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH
 var mySqlConfig = {
     host: "localhost",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    database: "StardewTestes",
+    user: "root",
+    password: "#Gf52589376820",
 };
 
 function executar(instrucao) {
